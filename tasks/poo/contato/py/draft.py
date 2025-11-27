137cb6f4bfe09c3aa286df1b381e0eeb8220d515
kauas <kauasoussa12345@gmail.com>
2025-11-27 10:11:35 -0300

    contato

--- tasks/poo/contato/py/draft.py
+++ tasks/poo/contato/py/draft.py
@@ -1,27 +1,99 @@
+from hashlib import algorithms_guaranteed
+from sys import prefix
+
+
 class Fone:
     def __init__(self, id: str, number: str):
         self.id = id
         self.number = number
 
-    def getId(self):
+    def isValid(self) -> bool:
+        valid = "0123456789()."
+        for caract in self.number:
+            if caract not in valid:
+                return False
+        return True
+
+    def getId(self) -> str:
         return self.id
-    def getNumber(self):
+
+    def getNumber(self) -> str:
         return self.number
+
     def setId(self, id: str):
         self.id = id
+
     def setNumber(self, number: str):
         self.number = number
-     def isValid(self) -> bool:
-         if
 
     def __str__(self):
-        return f"{self.id}, {self.number}"
+        return f"{self.id}:{self.number}"
+
 
 class Contact:
-    def __init__(self, name: str):
+    def __init__(self, name: str = ""):
         self.name = name
-        self.favorited : bool
-        self.fone: list[Fone] = []
+        self.favorited = False
+        self.fones: list[Fone] = []
 
     def addFone(self, id: str, number: str):
-        if
+        fone = Fone(id, number)
+        if fone.isValid():
+            self.fones.append(fone)
+        else:
+            print("fail: numero invalido")
+    def rmFone(self, index: int):
+        if 0 <= index < len(self.fones):
+            self.fones.pop(index)
+        else:
+            print("fail: index errado")
+    def toogleFavorited(self):
+        self.favorited = not self.favorited
+
+    def isFavorited(self) -> bool:
+        return self.favorited
+
+    def getFones(self):
+        return self.fones
+
+    def getName(self):
+        return self.name
+
+    def setName(self, name: str):
+        self.name = name
+
+    def __str__(self):
+        prefix = "@" if self.favorited else ""
+        lista = ", ".join([str(f) for f in self.fones])
+        return f"-{prefix} {self.name} [{lista}]"
+
+
+
+
+
+
+def main():
+    contact = Contact
+    while True:
+        line = input()
+        print("$" + line)
+        args = line.split()
+
+        if args[0] == "end":
+            break
+        elif args[0] == "init":
+            name = args[1] if len(args) > 1 else ""
+            contact = Contact(name)
+
+        elif args[0] == "show":
+            print(contact)
+        elif args[0] == "add":
+            id = args[1]
+            number = args[2]
+            contact.addFone(id, number)
+
+
+
+
+
+main()
